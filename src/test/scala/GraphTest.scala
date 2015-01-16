ceb17cb0d8ec27fefd9a9ff95920ae31fc0e266c
Hiro Kai <fullerenec84@gmail.com>
2015-01-15 17:00:54 -0800

    Add test images.

--- src/test/scala/GraphTest.scala
+++ src/test/scala/GraphTest.scala
@@ -4,29 +4,33 @@ import imagepipeline._
 import ij.IJ
 import ij.process.ImageProcessor
 import org.scalatest.FlatSpec
+import org.scalatest._
+
 
 import scalax.io.{Codec, Resource, Output}
 import Defs._
+import scala.util.Random
 
-class SetSpec extends FlatSpec {
+class SetSpec extends FlatSpec with Matchers {
 
   "getstats" should "not cause error" in {
     import Defs._
-    getstats.verify()
-    val res = Pipeline.run(getstats, Tuple1("/Users/hiroyuki/repos/ImagePipeline/BF.jpg"))
+    val res = getstats.run(Tuple1("./testimgs/BF.tif"))
     println(res.mkString(","))
   }
 
 
   "getstats_roi" should "be fine" in {
-    import Defs._
-    for(i <- 0 until 10){
-      printf("Repeating: %d\n",i)
+    val rng = new Random
+
+    for (i <- 0 until 10) {
+      val pos = (rng.nextInt(300), rng.nextInt(300), rng.nextInt(300), rng.nextInt(300))
+      printf("Repeating: %d\n", i)
       val getstats_roi: CompleteCalc = Pipeline.start(file_path).then(imload).then1(statroi, roi).output().interface(file_path, roi)
-      println(getstats_roi)
-      getstats_roi.verify()
-      val res = Pipeline.run(getstats_roi, ("/Users/hiroyuki/repos/ImagePipeline/BF.jpg", (0,0,300,300)))
-      println(res.mkString(","))
+      val res = Pipeline.run(getstats_roi, ("./testimgs/BF.tif", pos))
+
+      res.length shouldBe 1
+      res(0).isInstanceOf[RowData] shouldBe true
     }
   }
 
@@ -35,9 +39,9 @@ class SetSpec extends FlatSpec {
       import scala.sys.process._
       val g = cropAndCombine
       g.verify()
-      val img1 = IJ.openImage("/Users/hiroyuki/repos/ImagePipeline/BF.jpg").getProcessor
-      val img2 = IJ.openImage("/Users/hiroyuki/repos/ImagePipeline/Cy5.jpg").getProcessor
-      val res = Pipeline.run(g,(img1,img2,(0,0,400,300)))(0).asInstanceOf[ImageProcessor]
+      val img1 = IJ.openImage("./testimgs/BF.jpg").getProcessor
+      val img2 = IJ.openImage("./testimgs/Cy5.jpg").getProcessor
+      val res = Pipeline.run(g, (img1, img2, (0, 0, 400, 300)))(0).asInstanceOf[ImageProcessor]
       println(res)
       Seq("rm", "test.dot").mkString(" ").!
       val output: Output = Resource.fromFile("test.dot")
@@ -59,7 +63,7 @@ class MapNodeSpec extends FlatSpec {
   it should "run" in {
     val filelist = new InputFileList()
     val a: CompleteCalc = Pipeline.start(filelist).map(imload).output().interface(filelist)
-    a.run(Tuple1(Array("/Users/hiroyuki/repos/ImagePipeline/BF.jpg","/Users/hiroyuki/repos/ImagePipeline/Cy5.jpg")))
+    a.run(Tuple1(Array("/Users/hiroyuki/repos/ImagePipeline/BF.jpg", "/Users/hiroyuki/repos/ImagePipeline/Cy5.jpg")))
   }
 
 }
